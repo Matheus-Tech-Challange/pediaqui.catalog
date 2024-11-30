ccd5aef53b1463ac7017a787721d5a0fd1e1f849
Matheus Fernandes da Luz <matheusfernandesluz@gmail.com>
2024-11-30 13:50:43 -0400

    fix: ajusta nome bucket.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,7 +5,7 @@ terraform {
     local = ">=2.1.0"
   }
   backend "s3" {
-    bucket = "tc-api-bucket"
+    bucket = "pediaqui-catalog-bucket"
     key = "terraform.tfstate"
     region = "us-east-1"
   }
